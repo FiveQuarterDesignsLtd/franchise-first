009ee8051a49dbea8932d955dabb45aa51945da1
Anmol Nandha <anmol@fivequarterdesigns.com>
2018-04-25 13:39:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Franchise First
+
+## The purpose of this repo is to house the CSS animation used for this site.
---
+++ index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.typekit.net/nrx1onf.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Franchise Futures</title>
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div class="d-flex justify-content-center">
+				<img src="img/franchise-futures-logo@2x.png" alt="Franchise Futures - Helping you move forward">
+			</div>
+		</div>
+	</header>
+	<section class="content">
+		<div class="container">
+			<h1 class="text-center">Website coming soon...</h1>
+			<h2 class="text-center">In the meantime, if you’d like to get in touch<br class="hide"> please submit an enquiry using the form below</h2>
+		</div>
+	</section>
+	<section class="enquiry">
+		<div class="container d-flex justify-content-center">
+			<div class="enquiry-form">
+				<form method="post" action="thank-you.php" name="enquiry-form">
+					<div class="form-group">
+						<label for="txtFullName" class="sr-only">Full name*</label>
+						<input type="text" class="form-control" id="txtFullName" name="txtFullName" aria-describedby="fullNameHelp" placeholder="*Full name" required>
+					</div>
+					<div class="form-group">
+						<label for="txtEmail" class="sr-only">Email address*</label>
+						<input type="email" class="form-control" id="txtEmail" name="txtEmail" aria-describedby="emailHelp" placeholder="*Enter email" required>
+					</div>
+					<div class="form-group">
+						<label for="txtTelNo" class="sr-only">*Telephone Number</label>
+						<input type="tel" class="form-control" id="txtTelNo" name="txtTelNo" placeholder="*Telephone number" required>
+					</div>
+					<div class="form-group">
+						<span class="custom-dropdown">
+							<select required name="ddlIAm" id="ddlIAm" name="ddlIAm" class="form-control">
+								<option value="">*Nature of your enquiry...</option>
+								<option value="I am a Franchisor looking for support">I am a Franchisor looking for support</option>
+								<option value="I am a Franchisee looking for support">I am a Franchisee looking for support</option>
+								<option value="I am looking for a Franchise opportunity">I am looking for a Franchise opportunity</option>
+								<option value="I am looking to convert my business into a Franchise model">I am looking to convert my business into a Franchise model</option>
+								<option value="Other">Other</option>
+							</select>
+						</span>
+					</div>
+					<div class="form-group">
+						<label for="txtAdditionalInfo" class="sr-only">Additional info</label>
+						<textarea name="txtAdditionalInfo" id="txtAdditionalInfo" class="form-control" placeholder="Additional information"></textarea>
+					</div>
+					<div class="form-group">
+						<span class="custom-dropdown">
+							<select name="ddlBestTime" id="ddlBestTime" class="form-control" required>
+								<option value="">*Best time to call...</option>
+								<option value="Morning">Morning</option>
+								<option value="Afternoon">Afternoon</option>
+								<option value="Evening">Evening</option>
+								<option value="Anytime">Anytime</option>
+							</select>
+						</span>
+					</div>
+					<div class="form-group">
+						<label for="txtReferal" class="sr-only">*Where did you hear of us?</label>
+						<input type="text" class="form-control" id="txtReferal" name="txtReferal" aria-describedby="referalHelp" placeholder="*Where did you hear of us?" required>
+					</div>
+					<button type="submit" class="btn btn-ff btn-lg btn-block">SUBMIT</button>
+				</form>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			<p class="text-center">e: <a href="mailto:info@franchisefutures.co.uk">info@franchisefutures.co.uk</a><br>w: <a href="http://www.franchisefutures.co.uk">www.franchisefutures.co.uk</a></p>
+		</div>
+	</footer>
+
+	<!-- Javascript -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+
+</html>
